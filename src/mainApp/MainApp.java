56e8d9acf5352e606ef4a96d017e0c28f3ec429a
2tmb2 <tal@belkind.net>
2024-10-23 15:27:52 -0400

    Added Javadoc comments

--- src/mainApp/MainApp.java
+++ src/mainApp/MainApp.java
@@ -15,11 +15,12 @@ import javax.swing.JFrame;
 
 /**
  * Class: MainApp
- * @author Put your team name here
+ * @author F24_A304
  * <br>Purpose: Top level class for CSSE220 Project containing main method 
  * <br>Restrictions: None
  */
 public class MainApp implements KeyListener{
+	
 	private final Set<Integer> pressedKeys = new HashSet<>();
 	private LevelComponent lvl;
 	private JFrame frame;
@@ -27,12 +28,18 @@ public class MainApp implements KeyListener{
 	private int strawberryCount;
 	private int currentLevel;
 	private boolean strawberryAlreadyCollected;
+	
 	public MainApp() {
+		// sets default values
 		currentLevel = 1;
 		strawberryAlreadyCollected = false;
 		frame = new JFrame();
+		// adds this to the frame in order to listen for keyboard input
 		frame.addKeyListener(this);
+		// sets the preferred size of the inner contentPane to (768, 768)
+		// frame.setPreferredSize() includes the borders so it was not desirable
 		frame.getContentPane().setPreferredSize(new Dimension(frameSize, frameSize));
+		// make the frame non-resizable so that drawn objects remain in the correct position
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setBackground(Color.BLACK);
@@ -40,16 +47,23 @@ public class MainApp implements KeyListener{
 		lvl = new LevelComponent(this, currentLevel, strawberryAlreadyCollected);
 		frame.add(lvl);
 		frame.setVisible(true);
+		// creates a timer that fires every 10 milliseconds. This acts as our main game loop.
 		Timer t = new Timer(10, new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e)
 			{
 				updateMadelinePosition();
+				lvl.updateAnimations();
 				frame.repaint();
 			}
 		});
 		t.start();
 	}
+	
+	/**
+	 * Resets the current level to it's default state, 
+	 * with the exception of Strawberries and Breakable blocks which remain in their pre-reset state
+	 */
 	public void resetLevel()
 	{
 		frame.remove(lvl);
@@ -66,10 +80,18 @@ public class MainApp implements KeyListener{
 		t.start();
 		
 	}
+	
+	/**
+	 * Handles pressing a key
+	 */
 	@Override
     public synchronized void keyPressed(KeyEvent e) {
         pressedKeys.add(e.getKeyCode());
     }
+	
+	/**
+	 * Handles releasing a key
+	 */
     @Override
     public synchronized void keyReleased(KeyEvent e) {
     	if (e.getKeyCode() == 74)
@@ -78,21 +100,68 @@ public class MainApp implements KeyListener{
     	}
         pressedKeys.remove(e.getKeyCode());
     }
+    
     public void keyTyped(KeyEvent e) {}
-    public void updateMadelinePosition()
+    
+    /**
+     * Adds to the Strawberry counter and sets the strawberry in that level to collected
+     */
+    public void collectStrawberry()
     {
-    	lvl.checkIfDashing();
-    	lvl.moveMadelineVertically();
-    	lvl.moveMadelineHorizontally();
-    	lvl.updateAnimations();
+    	strawberryAlreadyCollected = true;
+    	strawberryCount++;
+    }
+    
+    /**
+     * Moves to the next level sequentially
+     */
+    public void nextLevel()
+    {
+    	strawberryAlreadyCollected = false;
+    	frame.remove(lvl);
+    	currentLevel++;
+    	lvl = new LevelComponent(this, currentLevel, strawberryAlreadyCollected);
+    	frame.add(lvl);
+    	frame.setVisible(true);
+    }
+    
+    /**
+     * Updates Madeline's current position
+     */
+    private void updateMadelinePosition()
+    {
+    	// 68 is d
     	if (pressedKeys.contains(68))
 		{
 			lvl.moveMadelineRight();
 		}
+    	// 65 is s
 		if (pressedKeys.contains(65))
 		{
 			lvl.moveMadelineLeft();
 		}
+		checkJump();
+    	checkDash();
+    	lvl.checkIfDashing();
+    	lvl.moveMadelineVertically();
+    	lvl.moveMadelineHorizontally();
+    }
+    
+    private void checkJump()
+    {
+    	// 74 is j
+    	if (pressedKeys.contains(74))
+		{
+			lvl.madelineJump();
+		}
+    }
+    
+    /**
+     * Checks if the dash button is being held. If it is, calls LevelComponent's Dash method in the appropriate direction
+     */
+    private void checkDash()
+    {
+		// 75 is k
 		if (pressedKeys.contains(75))
 		{
 			if (pressedKeys.contains(87) && pressedKeys.contains(68))
@@ -119,27 +188,14 @@ public class MainApp implements KeyListener{
 			{
 				lvl.dash("up");
 			}
-			lvl.dash("");
+			else
+			{
+				lvl.dash("");
+			}
+
 		}
-		if (pressedKeys.contains(74))
-		{
-			lvl.madelineJump();
-		}
     }
-    public void collectStrawberry()
-    {
-    	strawberryAlreadyCollected = true;
-    	strawberryCount++;
-    }
-    public void nextLevel()
-    {
-    	strawberryAlreadyCollected = false;
-    	frame.remove(lvl);
-    	currentLevel++;
-    	lvl = new LevelComponent(this, currentLevel, strawberryAlreadyCollected);
-    	frame.add(lvl);
-    	frame.setVisible(true);
-    }
+    
 	/**
 	 * ensures: runs the application
 	 * @param args unused
