0684ab40b872a324b74ab8e57b2a604c6783c603
2tmb2 <tal@belkind.net>
2024-10-23 19:12:38 -0400

    Create Javadoc comments for PointText

--- src/mainApp/PointText.java
+++ src/mainApp/PointText.java
@@ -1,6 +1,11 @@
 package mainApp;
 import java.awt.Color;
 import java.awt.Graphics2D;
+
+/**
+ * Class: PointText
+ * Purpose: Display the text 1000 upon collecting a strawberry
+ */
 public class PointText {
 	
 	private static final Color RED_TEXT = new Color(255, 0, 77);
@@ -10,6 +15,12 @@ public class PointText {
 	private int y;
 	private int numOfAnimationFrames;
 	private int translateBy;
+	
+	/**
+	 * Create a PointText object
+	 * @param x representing the center of where the text should spawn
+	 * @param y representing the center of where the text should spawn
+	 */
 	public PointText(int x, int y)
 	{
 		currentColor = RED_TEXT;
@@ -17,18 +28,23 @@ public class PointText {
 		this.x = x;
 		this.y = y;
 	}
+	
+	/**
+	 * Draws the text onto g2
+	 */
 	public void drawOn(Graphics2D g2)
 	{
+		// creates a copy of the Graphics2D object so that translations don't affect future drawings
 		g2 = (Graphics2D)g2.create();
 		g2.translate(x-36, y-translateBy-20);
 		g2.setColor(currentColor);
 		
-		//1
+		// draws a 1
 		g2.fillRect(0,0,6,6);
 		g2.fillRect(6, 0, 6, 6*4);
 		g2.fillRect(0, 4*6, 6*3, 6);
 		
-		//0
+		// draws 3 0's 24 pixels apart
 		for (int i = 0; i < 3; i++)
 		{
 			g2.translate(24, 0);	
@@ -38,6 +54,10 @@ public class PointText {
 			g2.fillRect(12, 0, 6, 24);
 		}
 	}
+	
+	/**
+	 * Swaps color to whatever color is currently inactive
+	 */
 	private void swapColor()
 	{
 		if (currentColor.equals(RED_TEXT))
@@ -49,6 +69,11 @@ public class PointText {
 			currentColor = RED_TEXT;
 		}
 	}
+	
+	/**
+	 * Updates the position and color of the PointText
+	 * @return true if the animation should stop, false if the animation should continue
+	 */
 	public boolean updateAnimation()
 	{
 		if (translateBy <= numOfAnimationFrames)
