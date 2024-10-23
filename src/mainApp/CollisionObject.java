f6ea1e1d4d50c75a65b36dc2314361ae74fb5cb3
2tmb2 <tal@belkind.net>
2024-10-23 16:03:00 -0400

    Added Javadoc comments for CollisionObject

--- src/mainApp/CollisionObject.java
+++ src/mainApp/CollisionObject.java
@@ -1,9 +1,12 @@
 package mainApp;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
-import java.io.Console;
 
+/**
+ * Class: CollisionObject <br>
+ * Purpose: Used to represent an object that the player character Madeline can
+ * collide with
+ */
 public class CollisionObject {
 	private int x;
 	private int y;
@@ -11,96 +14,117 @@ public class CollisionObject {
 	private int height;
 	private static final int MADELINE_WIDTH = 48;
 	private static final int MADELINE_HEIGHT = 42;
-	
+
 	public CollisionObject(int x, int y, int width, int height) {
 		this.x = x;
 		this.y = y;
 		this.width = width;
 		this.height = height;
 	}
-	public void drawOn(Graphics2D g2)
-	{
-		g2 = (Graphics2D) g2.create();
-		g2.setColor(Color.GRAY);
-		g2.fillRect(x, y, width, height);
-	}
-	public boolean isCollidingFloor(int madelineX, int madelineY)
-	{
-		if (madelineX + MADELINE_WIDTH - 10 > x && madelineX + 10 < x + width)
-		{
-			if (madelineY + MADELINE_HEIGHT > y)
-			{
-				if (madelineY + MADELINE_HEIGHT - y <= height)
-				{
+
+	public void drawOn(Graphics2D g2) {}
+
+	/**
+	 * Checks if the character located at the given x and y values would be within
+	 * the floor
+	 * 
+	 * @param madelineX representing the x position of madeline
+	 * @param madelineY representing the y position of madeline
+	 * @return true if Madeline would be colliding with the floor, otherwise returns
+	 *         false
+	 */
+	public boolean isCollidingFloor(int madelineX, int madelineY) {
+		if (madelineX + MADELINE_WIDTH - 10 > x && madelineX + 10 < x + width) {
+			if (madelineY + MADELINE_HEIGHT > y) {
+				if (madelineY + MADELINE_HEIGHT - y <= height) {
 					return true;
 				}
 			}
 		}
 		return false;
 	}
-	public boolean isCollidingCeiling(int madelineX, int madelineY)
-	{
-		if (madelineX + MADELINE_WIDTH - 10 > x && madelineX + 10 < x + width)
-		{
-			if (madelineY > y + height/2)
-			{
-				if (madelineY - (y+height) <= 0)
-				{
+
+	/**
+	 * Checks if the character located at the given x and y values would be within
+	 * the ceiling
+	 * 
+	 * @param madelineX representing the x position of madeline
+	 * @param madelineY representing the y position of madeline
+	 * @return true if Madeline would be colliding with the ceiling, otherwise
+	 *         returns false
+	 */
+	public boolean isCollidingCeiling(int madelineX, int madelineY) {
+		if (madelineX + MADELINE_WIDTH - 10 > x && madelineX + 10 < x + width) {
+			if (madelineY > y + height / 2) {
+				if (madelineY - (y + height) <= 0) {
 					return true;
 				}
 			}
 		}
 		return false;
 	}
+
+	/**
+	 * Checks if the character located at the given x and y values would be within
+	 * the wall
+	 * 
+	 * @param madelineX representing the x position of madeline
+	 * @param madelineY representing the y position of madeline
+	 * @return true if Madeline would be colliding with the wall, otherwise returns
+	 *         false
+	 */
 	public boolean isCollidingWall(int madelineX, int madelineY, int facing) {
-		if (madelineY + MADELINE_HEIGHT > y + 5 && madelineY < y + height - 5)
-		{
-			// facing left, to the right
-			if (facing < 0 && madelineX > x + width/2)
-			{
-				if (madelineX - (x+width) <= 0)
-					//System.out.println("yes");
+		// ensures that madeline is below the top of the object and above the bottom of
+		// the object
+		if (madelineY + MADELINE_HEIGHT > y + 5 && madelineY < y + height - 5) {
+			// if madeline is facing left and moving to the right
+			if (facing < 0 && madelineX > x + width / 2) {
+				if (madelineX - (x + width) <= 0)
 					return true;
 			}
-			// facing right, to the right
-			else if (facing > 0 && madelineX > x + width/2)
-			{
-				if (madelineX + MADELINE_WIDTH - (x+width) <= 0)
+			// if madeline is facing right and moving to the right
+			else if (facing > 0 && madelineX > x + width / 2) {
+				if (madelineX + MADELINE_WIDTH - (x + width) <= 0)
 					return true;
 			}
-			// facing left, to the left
-			else if (facing < 0 && madelineX < x + width/2)
-			{
+			// if madeline is facing left and moving to the left
+			else if (facing < 0 && madelineX < x + width / 2) {
 				if (madelineX - x >= 0)
 					return true;
 			}
-			// facing right, to the left
-			else if (facing > 0 && madelineX + MADELINE_WIDTH < x + width/2)
-			{
+			// if madeline is facing right and moving to the left
+			else if (facing > 0 && madelineX + MADELINE_WIDTH < x + width / 2) {
 				if (madelineX + MADELINE_WIDTH - x >= 0)
 					return true;
 			}
 		}
 		return false;
 	}
-	public int getWidth()
-	{
+
+	public int getWidth() {
 		return width;
 	}
-	public int getHeight()
-	{
+
+	public int getHeight() {
 		return height;
 	}
-	public int getX()
-	{
+
+	public int getX() {
 		return x;
 	}
-	public int getY()
-	{
+
+	public int getY() {
 		return y;
 	}
-	public void updateAnimation() {}
+
+	public void updateAnimation() {
+	}
+
+	/**
+	 * Returns a String representing the CollisionObject in the following format:
+	 * Class, x y width height
+	 */
 	public String toString() {
-		return this.getClass().toString().substring(6) + ", " + this.getX() + " " + this.getY() + " " + this.getWidth() + " " + this.getHeight() + " ";
+		return this.getClass().toString().substring(6) + ", " + this.getX() + " " + this.getY() + " " + this.getWidth() + " " + this.getHeight();
 	}
 }
