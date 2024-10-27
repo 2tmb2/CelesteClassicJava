4aa43188b91fefe9372670c5f70f60bd9360cc5f
weyermg <weyermg@RHIT-PW0BFM6B.rose-hulman.edu>
2024-10-27 17:59:55 -0400

    refactored spike class code

---
+++ src/mainApp/RightSpike.java
@@ -0,0 +1,22 @@
+package mainApp;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+public class RightSpike extends Spike {
+
+	private static final Color SPIKE_WHITE = new Color(255,241,232);
+	private static final Color SPIKE_GREY = new Color(194,195,199);
+	
+	public RightSpike(int x, int y, int height, Madeline m)
+	{
+		super(x,y,1,height,
+				m);
+	}
+	@Override
+	public void drawOn(Graphics2D g2)
+	{
+		
+	}
+
+}
--- src/mainApp/UpSpike.java
+++ src/mainApp/UpSpike.java
@@ -3,14 +3,14 @@ package mainApp;
 import java.awt.Color;
 import java.awt.Graphics2D;
 
-public class HorizontalSpike extends Spike {
+public class UpSpike extends Spike {
 	
 	private static final Color SPIKE_WHITE = new Color(255,241,232);
 	private static final Color SPIKE_GREY = new Color(194,195,199);
 	
-	public HorizontalSpike(int x, int y, int width, int height, Madeline m)
+	public UpSpike(int x, int y, int width, Madeline m)
 	{
-		super(x,y,width,height, m);
+		super(x,y,width,1, m);
 	}
 	@Override
 	public void drawOn(Graphics2D g2)
