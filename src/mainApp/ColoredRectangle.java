e334b01fe227fc8683ca0f786fcc678a4a2b5eb8
MatthewWeyer <mattdaggerchin@gmail.com>
2024-10-29 23:02:07 -0400

    Added Collider Functionality to Sprite Drawer

---
+++ src/mainApp/ColoredRectangle.java
@@ -0,0 +1,27 @@
+package mainApp;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.Rectangle;
+
+public class ColoredRectangle extends Rectangle {
+	Color color;
+	public ColoredRectangle(Point p, Dimension d, Color color) {
+		super(p, d);
+		this.color = color;
+	}
+	
+	public ColoredRectangle(ColoredRectangle r) {
+		super(r.getLocation(), r.getSize());
+		this.color = r.getColor();
+	}
+	
+	public Color getColor() {
+		return this.color;
+	}
+	
+	public void setColor(Color color) {
+		this.color = color;
+	}
+}
--- src/mainApp/LevelEditor.java
+++ src/mainApp/LevelEditor.java
@@ -1,17 +1,23 @@
 package mainApp;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.Stroke;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 import javax.swing.JComponent;
 
 
 public class LevelEditor extends JComponent {
+	
 	private BufferedImage map;
 	private BufferedImage scaledMap;
 	private BufferedImage confirm;
@@ -19,13 +25,20 @@ public class LevelEditor extends JComponent {
 	private BufferedImage checkmark;
 	private BufferedImage ex;
 	private BufferedImage grid;
-	private static final int ATLAS_WIDTH = 128 * 6;
-	private static final int ATLAS_HEIGHT = 88 * 6;
-	private static final int OPTIONS_Y = 16 * 6;
+	
+	private static final int ATLAS_WIDTH = 128 * MainApp.PIXEL_DIM;
+	private static final int ATLAS_HEIGHT = 88 * MainApp.PIXEL_DIM;
+	private static final int OPTIONS_Y = 16 * MainApp.PIXEL_DIM;
 	private static final int GAME_WIDTH = 768;
 	private static final int GAME_HEIGHT = GAME_WIDTH;
 	private static final int SPRITE_WIDTH = 48;
 	private static final int SPRITE_HEIGHT = SPRITE_WIDTH;
+	
+	private static final Color GREEN_COLOR = new Color(14,209, 69);
+	private static final Color BLUE_COLOR = new Color(63, 73, 204);
+	private static final int COLLIDER_THICKNESS = 4;
+	private static final BasicStroke RECT_STROKE = new BasicStroke(COLLIDER_THICKNESS);
+	
 	private int gridX = 0;
 	private int gridY = 0;
 	private int selectedX = GAME_WIDTH;
@@ -34,12 +47,18 @@ public class LevelEditor extends JComponent {
 	private int newLayer = GAME_WIDTH;
 	private int selectedLayer = 0;
 	private int newSelectedLayer = 0;
+	
+	private Point topLeft = null;
+	private Point botRight = null;
+	private ColoredRectangle tempRect;
+	private ArrayList<ColoredRectangle> colliders = new ArrayList<ColoredRectangle>(); //Collision Layer
+	
 	private Point[][] layer1 = new Point[16][16]; //Background Layer
 	private Point[][] layer2 = new Point[16][16]; //Foreground Layer
 	private Point[][] layer3 = new Point[16][16]; //Detail Layer
-	private Point[][] layer4 = new Point[16][16]; //Collision Layer
+	//private Point[][] layer4 = new Point[16][16]; //Collision Layer
 	private Point[][] layer5 = new Point[16][16]; //Object Layer
-	private Boolean[] renderLayers = new Boolean[] {true,true,true,true,true,true};
+	private Boolean[] renderLayers = new Boolean[] {true,true,true,true,true,false};
 	public LevelEditor() {
 		try {
 			map = ImageIO.read(new File("src/Sprites/atlas.png"));
@@ -87,8 +106,11 @@ public class LevelEditor extends JComponent {
 		if (renderLayers[2]) {
 			drawLayer(g2, layer3);
 		}
+		if (tempRect != null) {
+			drawRectangle(g2, tempRect);
+		}
 		if (renderLayers[3]) {
-			drawLayer(g2, layer4);
+			drawColliders(g2, colliders);
 		}
 		if (renderLayers[4]) {
 			drawLayer(g2, layer5);
@@ -104,9 +126,23 @@ public class LevelEditor extends JComponent {
 			for (int j = 0; j < layer[0].length; j++) {
 				if (layer[i][j] == null) continue;
 				//For some reason there is a vertical offset of 1 when drawing the sprites, so the source y1 is increased by 1
-				g.drawImage(scaledMap, i * SPRITE_WIDTH, j * SPRITE_HEIGHT, i * SPRITE_WIDTH + SPRITE_WIDTH, j * SPRITE_HEIGHT + SPRITE_HEIGHT, (layer[i][j].getX() - GAME_WIDTH), layer[i][j].getY() + 1, ((layer[i][j].getX() - GAME_WIDTH)) + SPRITE_WIDTH, (layer[i][j].getY()) + SPRITE_HEIGHT, null);
+				g.drawImage(scaledMap, i * SPRITE_WIDTH, j * SPRITE_HEIGHT, i * SPRITE_WIDTH + SPRITE_WIDTH, j * SPRITE_HEIGHT + SPRITE_HEIGHT, ((int)layer[i][j].getX() - GAME_WIDTH), (int)layer[i][j].getY() + 1, (((int)layer[i][j].getX() - GAME_WIDTH)) + SPRITE_WIDTH, ((int)layer[i][j].getY()) + SPRITE_HEIGHT, null);
 			}
 		}
+	}
+	
+	public void drawColliders(Graphics2D g, ArrayList<ColoredRectangle> colliders) {
+		for (ColoredRectangle r : colliders) {
+			drawRectangle(g, r);
+		}
+	}
+	
+	public void drawRectangle(Graphics2D g2, ColoredRectangle r) {
+		Stroke oldStroke = g2.getStroke();
+		g2.setColor(r.getColor());
+		g2.setStroke(RECT_STROKE);
+		g2.drawRect((int)r.getX() + (COLLIDER_THICKNESS / 2), (int)r.getY() + (COLLIDER_THICKNESS / 4), (int)r.getWidth() - (COLLIDER_THICKNESS ), (int)r.getHeight() - (COLLIDER_THICKNESS));
+		g2.setStroke(oldStroke);
 	}
 	
 	public void doMouseClick(int x, int y) {
@@ -158,17 +194,44 @@ public class LevelEditor extends JComponent {
 			}
 			
 		}
-		else if (x <= GAME_WIDTH) {
+		else if (x <= GAME_WIDTH && selectedLayer != 4) {
 			if (selectedLayer == 1) {
 				layer1[x / SPRITE_WIDTH][y / SPRITE_WIDTH] = new Point(selectedX, selectedY);
 			} else if (selectedLayer == 2) {
 				layer2[x / SPRITE_WIDTH][y / SPRITE_WIDTH] = new Point(selectedX, selectedY);
 			} else if (selectedLayer == 3) {
 				layer3[x / SPRITE_WIDTH][y / SPRITE_WIDTH] = new Point(selectedX, selectedY);
-			} else if (selectedLayer == 4) {
-				layer4[x / SPRITE_WIDTH][y / SPRITE_WIDTH] = new Point(selectedX, selectedY);
 			} else if (selectedLayer == 5) {
 				layer5[x / SPRITE_WIDTH][y / SPRITE_WIDTH] = new Point(selectedX, selectedY);
+			}
+		} else if (x <= GAME_WIDTH && selectedLayer == 4) {
+			topLeft = new Point(x - (x % SPRITE_WIDTH), y - (y % SPRITE_HEIGHT));
+			botRight = new Point();
+			tempRect = new ColoredRectangle(topLeft, new Dimension((int)(botRight.getX() - topLeft.getX()),(int)(botRight.getX() - topLeft.getX())), GREEN_COLOR);
+		}
+	}
+	
+	public void doMouseRelease(int x, int y) {
+		x -= 8;
+		y -= 30;
+		if (tempRect != null) {
+			colliders.add(new ColoredRectangle(tempRect));
+			tempRect = null;
+		}
+		
+	}
+	
+	public void doMouseHold(int x, int y) {
+		x -= 8;
+		y -= 30;
+		int botRightX;
+		int botRightY;
+		if (topLeft != null) {
+			botRightX = x + (SPRITE_WIDTH - (x % SPRITE_WIDTH));
+			botRightY = y + (SPRITE_HEIGHT - (y % SPRITE_HEIGHT));
+			botRight.setLocation(botRightX, botRightY);
+			if (tempRect != null) {
+				tempRect.setSize((int)(botRight.getX() - topLeft.getX()),(int)(botRight.getY() - topLeft.getY()));
 			}
 			
 		}
@@ -185,7 +248,7 @@ public class LevelEditor extends JComponent {
 			layer3 = new Point[16][16];
 		}
 		if (renderLayers[3]) {
-			layer4 = new Point[16][16];
+			colliders.clear();
 		}
 		if (renderLayers[4]) {
 			layer5 = new Point[16][16];
