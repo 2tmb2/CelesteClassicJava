faa4a9aa97fbc9aa8272dcbcdaeab40f97f35093
weyermg <weyermg@RHIT-PW0BFM6B.rose-hulman.edu>
2024-11-05 09:39:20 -0500

    Fixed Left Spike hitbox

--- src/spikes/LeftSpike.java
+++ src/spikes/LeftSpike.java
@@ -11,15 +11,17 @@ public class LeftSpike extends Spike {
 	private static final Color SPIKE_WHITE = new Color(255, 241, 232);
 	private static final Color SPIKE_GREY = new Color(194, 195, 199);
 	private static final Color SPIKE_BROWN = new Color(95, 87, 79);
+	
+	private static final int MADELINE_COLLISION_WIDTH = 30;
 
 	public LeftSpike(int x, int y, int height, Madeline m) {
-		super(x, y, 3*MainApp.PIXEL_DIM, height, m);
+		super(x, y + 1, 3*MainApp.PIXEL_DIM, height, m);
 	}
 
 	@Override
 	public void drawOn(Graphics2D g2) {
 		g2 = (Graphics2D) g2.create();
-		g2.translate(getX() - 3*MainApp.PIXEL_DIM, getY() + MainApp.PIXEL_DIM);
+		g2.translate(getX() - 3*MainApp.PIXEL_DIM, getY() - 1 + MainApp.PIXEL_DIM);
 		for (int i = 0; i < getHeight() / (4*MainApp.PIXEL_DIM); i++) {
 			g2.setColor(SPIKE_GREY);
 			g2.fillRect(3*MainApp.PIXEL_DIM, 0, 3*MainApp.PIXEL_DIM, 3*MainApp.PIXEL_DIM);
