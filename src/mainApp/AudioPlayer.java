a767ade9040e89ec1ed18455c02a91f4640bebe9
2tmb2 <tal@belkind.net>
2024-11-08 00:18:29 -0500

    Added javadoc comments

--- src/mainApp/AudioPlayer.java
+++ src/mainApp/AudioPlayer.java
@@ -9,8 +9,19 @@ import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
+/**
+ * REQUIRED HELP CITATION
+ * 
+ * I utilized the following stack overflow answer for the basic structure of reading audio files
+ * https://stackoverflow.com/questions/8979914/audio-clip-wont-loop-continuously/8980146#8980146
+ */
+
 public class AudioPlayer {
 
+	/**
+	 * Plays an audio file based on the file name
+	 * @param audioFile representing the name of a .wav file located in src/AudioFiles/
+	 */
 	public static void playFile(String audioFile)
 	{
 		try {
@@ -28,6 +39,11 @@ public class AudioPlayer {
 		}
 	}
 	
+	/**
+	 * Plays an audio file based on the file name
+	 * @param audioFile representing the name of a .wav file located in src/AudioFiles/
+	 * @param loopAmount representing the number of times the audio should loop (for infinit, use Clip.LOOP_CONTINUOUSLY
+	 */
 	public static void playFile(String audioFile, int loopAmount)
 	{
 		try {
